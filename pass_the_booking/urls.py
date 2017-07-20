b7c2dead5d26b0382008155db715b5adccb551b5
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-07-20 14:02:14 +0100

    create form for adding new clients

--- pass_the_booking/urls.py
+++ pass_the_booking/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.client_list, name='client_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.client_detail, name='client_detail'),
+    url(r'^client/(?P<pk>\d+)/$', views.client_detail, name='client_detail'),
+    url(r'^client/new/$', views.client_new, name='client_new'),
 ]
--- pass_the_booking/views.py
+++ pass_the_booking/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from .models import Client
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import ClientForm
 
 def client_list(request):
     clients = Client.objects.order_by('name')
@@ -9,3 +9,14 @@ def client_list(request):
 def client_detail(request, pk):
     client = get_object_or_404(Client, pk=pk)
     return render(request, 'pass_the_booking/client_detail.html', {'client': client})
+
+def client_new(request):
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            client = form.save(commit=False)
+            client.save()
+            return redirect('client_detail', pk=client.pk)
+    else:
+        form = ClientForm()
+        return render(request, 'pass_the_booking/client_edit.html', {'form': form})
