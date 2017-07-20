c5ce62b9f62f7540e11088b530654a0540bdb06b
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-07-20 12:52:36 +0100

    create new view showing client details

--- pass_the_booking/urls.py
+++ pass_the_booking/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.client_list, name='client_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.client_detail, name='client_detail'),
 ]
--- pass_the_booking/views.py
+++ pass_the_booking/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Client
-
-
+from django.shortcuts import render, get_object_or_404
 
 def client_list(request):
     clients = Client.objects.order_by('name')
     return render(request, 'pass_the_booking/client_list.html', {'clients': clients})
+
+def client_detail(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    return render(request, 'pass_the_booking/client_detail.html', {'client': client})
