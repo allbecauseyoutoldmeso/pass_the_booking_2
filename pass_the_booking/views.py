c79641f91b493a2361afdc656371b1ad853e8aa4
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-07-20 11:52:18 +0100

    add list of clients to homepage

--- pass_the_booking/views.py
+++ pass_the_booking/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Client
+
+
 
 def client_list(request):
-    return render(request, 'pass_the_booking/client_list.html', {})
+    clients = Client.objects.order_by('name')
+    return render(request, 'pass_the_booking/client_list.html', {'clients': clients})
