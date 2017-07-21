d88d976b2a7612bad85951d664a6518c7483f506
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-07-21 19:58:17 +0100

    add feature tests for booking and property lists

--- pass_the_booking/tests/test_views.py
+++ pass_the_booking/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from ..models import Client, Property, Booking
 
 class ProjectTests(TestCase):
 
@@ -11,3 +12,16 @@ class ProjectTests(TestCase):
         Client.objects.create(name='kate gleeson', dob='1981-09-13', email='kate@kate.com', telephone='01234123123')
         response = self.client.get('/clients/')
         self.assertContains(response, 'kate gleeson')
+
+    def test_property_list(self):
+        kate = Client.objects.create(name='kate gleeson', dob='1981-09-13', email='kate@kate.com', telephone='01234123123')
+        Property.objects.create(client = kate, address='123 langthorne road', price=29, bedrooms=2, internet=False)
+        response = self.client.get('/properties/')
+        self.assertContains(response, '123 langthorne road')
+
+    def test_booking_list(self):
+        kate = Client.objects.create(name='kate gleeson', dob='1981-09-13', email='kate@kate.com', telephone='01234123123')
+        langthorne_road = Property.objects.create(client = kate, address='123 langthorne road', price=29, bedrooms=2, internet=False)
+        Booking.objects.create(property = langthorne_road, check_in = '2017-10-12', check_out = '2017-10-14', guest_name = 'sally', guest_email = 'sally@sally.com')
+        response = self.client.get('/bookings/')
+        self.assertContains(response, "sally's booking for 123 langthorne road")
