e944b78d88aa915cf4f3dc196e6be396ae14c0f1
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-07-21 15:44:04 +0100

    remove redundant code

--- pass_the_booking/views/__init__.py
+++ pass_the_booking/views/__init__.py
@@ -1,7 +1,3 @@
-from ..models import Client, Property, Booking
-from django.shortcuts import render, get_object_or_404, redirect
-from ..forms import ClientForm, PropertyForm, BookingForm
-
 from .clients import *
 from .properties import *
 from .bookings import *
