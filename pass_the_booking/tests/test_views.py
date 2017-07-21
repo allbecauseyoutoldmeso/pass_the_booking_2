fa9826e6bd092176b0cbaa0a0522c8b92b397f3f
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-07-21 22:49:43 +0100

    add test for booking detail view

--- pass_the_booking/tests/test_views.py
+++ pass_the_booking/tests/test_views.py
@@ -36,3 +36,11 @@ class ViewTests(TestCase):
         Booking.objects.create(property = langthorne_road, check_in = '2017-10-12', check_out = '2017-10-14', guest_name = 'sally', guest_email = 'sally@sally.com')
         response = self.client.get('/bookings/')
         self.assertContains(response, "sally's booking for 123 langthorne road")
+
+    def test_booking_detail(self):
+        kate = Client.objects.create(name='kate gleeson', dob='1981-09-13', email='kate@kate.com', telephone='01234123123')
+        langthorne_road = Property.objects.create(client = kate, address='123 langthorne road', price=29, bedrooms=2, internet=False)
+        Booking.objects.create(property = langthorne_road, check_in = '2017-10-12', check_out = '2017-10-14', guest_name = 'sally', guest_email = 'sally@sally.com')
+        response = self.client.get('/bookings/1/')
+        self.assertContains(response, '123 langthorne road')
+        self.assertContains(response, 'guest: sally')
