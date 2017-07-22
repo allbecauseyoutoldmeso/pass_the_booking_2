b429b19a5f01acb5b786af9e66a57be246950f40
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-07-22 10:09:57 +0100

    add tests for property form

--- pass_the_booking/tests/test_forms.py
+++ pass_the_booking/tests/test_forms.py
@@ -22,10 +22,37 @@ class ClientFormTest(TestCase):
     def test_blank_data(self):
         form = ClientForm({})
         self.assertFalse(form.is_valid())
-        print(form.errors)
         self.assertEqual(form.errors, {
             'name': ['This field is required.'],
             'dob': ['This field is required.'],
             'email': ['This field is required.'],
             'telephone': ['This field is required.'],
+        })
+
+class PropertyFormTest(TestCase):
+
+    def test_valid_data(self):
+        form = PropertyForm({
+            'address': '123 langthorne road',
+            'price': 29,
+            'bedrooms': 2,
+            'internet': True
+        })
+        self.assertTrue(form.is_valid())
+        property = form.save(commit=False)
+        property.client = Client.objects.create(name='kate gleeson', dob='1981-09-13', email='kate@kate.com', telephone='01234123123')
+        property.save()
+        self.assertEqual(property.address, '123 langthorne road')
+        self.assertEqual(property.price, 29)
+        self.assertEqual(property.bedrooms, 2)
+        self.assertEqual(property.internet, True)
+
+    def test_blank_data(self):
+        form = PropertyForm({})
+        self.assertFalse(form.is_valid())
+        print(form.errors)
+        self.assertEqual(form.errors, {
+            'address': ['This field is required.'],
+            'price': ['This field is required.'],
+            'bedrooms': ['This field is required.'],
         })
