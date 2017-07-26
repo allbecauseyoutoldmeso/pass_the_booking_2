3157f1518e414cbb002dc8e25a29915f0721ad5a
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-07-26 08:58:29 +0100

    use class based view for new client form

--- pass_the_booking/views/clients.py
+++ pass_the_booking/views/clients.py
@@ -1,7 +1,8 @@
-from ..models import Client, Property, Booking
+from ..models import Client
 from django.shortcuts import render, get_object_or_404, redirect
-from ..forms import ClientForm, PropertyForm, BookingForm
+from ..forms import ClientForm
 from django.views import View
+from django.http import HttpResponseRedirect
 
 class ClientListView(View):
     def get(self, request):
@@ -13,16 +14,21 @@ def client_detail(request, pk):
     properties = client.property_set.all()
     return render(request, 'pass_the_booking/clients/client_detail.html', {'client': client, 'properties': properties})
 
-def client_new(request):
-    if request.method == 'POST':
-        form = ClientForm(request.POST)
+class ClientNewView(View):
+    form_class = ClientForm
+    template_name = 'pass_the_booking/clients/client_edit.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
         if form.is_valid():
             client = form.save(commit=False)
             client.save()
             return redirect('client_detail', pk=client.pk)
-    else:
-        form = ClientForm()
-        return render(request, 'pass_the_booking/clients/client_edit.html', {'form': form})
+        return render(request, self.template_name, {'form': form})
 
 def client_edit(request, pk):
     client = get_object_or_404(Client, pk=pk)
