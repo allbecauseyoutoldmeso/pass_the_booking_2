ff7bf7e036b9125c61495a2d9e4fad4d290ce770
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-07-26 14:55:01 +0100

    booking detail view

--- pass_the_booking/views/bookings.py
+++ pass_the_booking/views/bookings.py
@@ -21,41 +21,13 @@ class BookingUpdate(UpdateView):
     fields = ['property', 'guest_name', 'guest_email', 'check_in', 'check_out']
     template_name = 'pass_the_booking/object_edit.html'
 
+class BookingDetailView(View):
+    def get(self, request, pk):
+        booking = get_object_or_404(Booking, pk=pk)
+        return render(request, 'pass_the_booking/bookings/booking_detail.html', {'booking': booking })
 
 
 
-def booking_detail(request, pk):
-    booking = get_object_or_404(Booking, pk=pk)
-    return render(request, 'pass_the_booking/bookings/booking_detail.html', {'booking': booking })
-
-def booking_new(request, pk):
-    property = get_object_or_404(Property, pk=pk)
-    if request.method == "POST":
-        form = BookingForm(request.POST)
-        if form.is_valid():
-            booking = form.save(commit=False)
-            booking.property = property
-            for date in booking.all_dates():
-                if date in property.unavailable_dates():
-                    messages.add_message(request, messages.INFO, 'Some of the dates you selected are not available.')
-                    return redirect('booking_new', pk=property.pk)
-            booking.save()
-            return redirect('booking_detail', pk=booking.pk)
-    else:
-        form = BookingForm()
-    return render(request, 'pass_the_booking/bookings/booking_edit.html', { 'form': form, 'property': property })
-
-def booking_edit(request, pk):
-    booking = get_object_or_404(Booking, pk=pk)
-    property = booking.property
-    if request.method == "POST":
-        form = BookingForm(request.POST, instance=booking)
-        if form.is_valid():
-            booking = form.save(commit=True)
-            return redirect('booking_detail', pk=booking.pk)
-    else:
-        form = BookingForm(instance=booking)
-    return render(request, 'pass_the_booking/bookings/booking_edit.html', {'form': form, 'property': property })
 
 def booking_delete(request, pk):
     booking = get_object_or_404(Booking, pk=pk)
