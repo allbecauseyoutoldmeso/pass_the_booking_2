fe7be38c32540d4968fd4b655a1612fb9b3ac438
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-07-26 09:20:20 +0100

    use generic list view for client list

--- pass_the_booking/views/clients.py
+++ pass_the_booking/views/clients.py
@@ -2,24 +2,17 @@ from ..models import Client
 from django.shortcuts import render, get_object_or_404, redirect
 from ..forms import ClientForm
 from django.views import View
-from django.http import HttpResponseRedirect
+from django.views.generic import ListView
 
-class ClientListView(View):
-    def get(self, request):
-        clients = Client.objects.order_by('name')
-        return render(request, 'pass_the_booking/clients/client_list.html', {'clients': clients})
+class ClientListView(ListView):
+    model = Client
+    template_name = 'pass_the_booking/clients/client_list.html'
 
 class ClientDetailView(View):
     def get(self, request, pk):
         client = get_object_or_404(Client, pk=pk)
         properties = client.property_set.all()
         return render(request, 'pass_the_booking/clients/client_detail.html', {'client': client, 'properties': properties})
-
-
-def client_detail(request, pk):
-    client = get_object_or_404(Client, pk=pk)
-    properties = client.property_set.all()
-    return render(request, 'pass_the_booking/clients/client_detail.html', {'client': client, 'properties': properties})
 
 class ClientNewView(View):
     form_class = ClientForm
